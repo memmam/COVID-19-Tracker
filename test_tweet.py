3c850fa1abb0768e44bba987ec05651a722ba2ae
TheYadda <mdmoriss@ucsd.edu>
2020-02-01 22:11:01 -0800

    test script

---
+++ test_tweet.py
@@ -0,0 +1,24 @@
+# Test script for debugging cron, posts a test message with a timestamp
+
+import time
+import tweepy
+import requests
+import json
+
+# Import our Twitter credentials from credentials.py
+from credentials import *
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create API object
+    wait_on_rate_limit_notify=True)
+api = tweepy.API(auth, wait_on_rate_limit=True,
+
+# Get current UTC time
+utctime = time.gmtime()
+
+# Build tweet string
+test_tweet = """ AUTOMATED POST TEST
+{utctime.tm_year}-{utctime.tm_mon}-{utctime.tm_mday} {utctime.tm_hour}:{utctime.tm_min}:{utctime.tm_sec} UTC"""
